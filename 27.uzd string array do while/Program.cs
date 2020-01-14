a37d98cfb47d3e290a28a010fb0e3d3a06f0183c
AivarsB <aivars.berzins@inbox.lv>
2020-01-14 16:31:56 +0200

    Add project files.

---
+++ 27.uzd string array do while/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace _27.uzd_string_array_do_while
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] names = new string[] { "Janis", "Peteris", "Gusts", "Aivars", "Juris" };
+
+            int n = 0;
+            int m = 0;
+
+            do
+            {
+                Console.WriteLine(names[m]);
+                n++;
+                m++;
+            }
+            while (n < names.Length);
+        }
+    }
+}
+// Izveidot console aplikāciju, kurā ir definēts string tipa masīvs ar cilvēku vārdiem. Izmantojot do while ciklu izdrukāt visus vārdus.
